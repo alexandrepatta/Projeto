12efc31a8539079f96763f61b52c711a59d848b3
Alexandre Patta <alexandrelpf@hotmail.com>
2023-12-13 18:30:06 -0300

    button changes

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function toggleMode() {
   const html = document.documentElement
-  
+
   html.classList.toggle("light")
+
+  if(html.classList.contains("light") == false){
+    html.classList.add("dark")
+  } else {
+    html.classList.remove("dark")
+  }
+
 }
