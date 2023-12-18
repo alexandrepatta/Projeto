d5cc3fadf9a86ed5603842592fc4b9be4e82ebcd
Alexandre Luís <alexandrelpf@hotmail.com>
2023-12-18 20:04:56 -0300

    add Email-me

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function toggleMode() {
   }
 
 }
+
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_7pmpfa6";
+  const templateID = "template_d149e8q";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(
+      res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
